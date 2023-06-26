274c10ac92ac48d9d29bf7c84b458e6adf8d95f1
LuisEsteban-tec <137138722+LuisEsteban-tec@users.noreply.github.com>
2023-06-26 10:47:38 -0700

    Update Calculadora.java

--- miPrincipal/Calculadora.java
+++ miPrincipal/Calculadora.java
@@ -3,18 +3,18 @@ package miPrincipal;
 public class Calculadora {
 
     public int suma(int x, int y) {
-        return x + y;
+        return 0;
     }
     
     public int resta(int x, int y) {
-        return x-y;
+        return 0;
     }
 
     public int multiplica(int x, int y) {
-        return x * y;
+        return 0;
     }
 
     public int divide(int x, int y) {
-        return x / y;
+        return 0;
     }
 }
